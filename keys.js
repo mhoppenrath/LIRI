35bec2478b4a9248a9aa9f7adbc040a4748fb09a
mhoppenrath <mmhoppenrath@gmail.com>
2017-09-02 16:07:47 -0500

    Inital push with most starting files

---
+++ keys.js
@@ -0,0 +1,7 @@
+
+  exports.twitterKeys = {
+    consumer_key: '0X3zrSkfvryKDFKevUhUoTgUI',
+    consumer_secret: 'ekpJVa77fDte2kjyaFWhj99Cq9DSQ6Enfa4U6XKcpfzryQBiux',
+    access_token_key: '904081013080514561-bIUfd17Nh5npFp8h69ikqN4SmYUuoKo',
+    access_token_secret: 'lgBdHjQZzIUVkuxBz5cqnS1CtX5D4NCNWjaPklfK0GOiz',
+  }
---
+++ liri.js
@@ -0,0 +1,17 @@
+var keys = require("./keys.js");
+var twitter = require('twitter');
+
+
+var programUsed = process.argv[2];
+var whatCalled = process.argv[3];
+
+function twitterCall(){
+
+}
+
+function spotifyCall(){
+
+}
+function OMDBCall(){
+  
+}
